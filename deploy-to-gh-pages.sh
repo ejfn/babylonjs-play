470e67c1c4ea83361fc99b67dd2b0b5fa2485459
eric <ericvan76@gmail.com>
2016-02-05 13:28:43 +1030

    attempt to fix travis build

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -15,4 +15,4 @@ cd dist
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
